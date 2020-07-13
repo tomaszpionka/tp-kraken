2bda139280db963160f3a2243498ed09ba878c39
tomaszpionka <57723049+tomaszpionka@users.noreply.github.com>
2020-07-13 19:29:38 +0200

    run build

---
+++ build/precache-manifest.beeb9d0c43a94d2d3e1a4c38d91d89af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f178e77832caa32e38ac90cc4eebbb2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3ae6fe7238d98679f8c",
+    "url": "/static/js/2.f88c08fd.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.f88c08fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8314f416649f692f8429",
+    "url": "/static/js/main.8970dba7.chunk.js"
+  },
+  {
+    "revision": "dcc97b6fe6e8f8b9f9a6",
+    "url": "/static/js/runtime-main.20e70669.js"
+  }
+]);
